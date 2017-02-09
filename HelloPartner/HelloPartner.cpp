70737b1318507fd20f88cfebe5e04d129cd76bc2
oscarmasferrerrubio <oscarmasferrerrubio@S1-01>
2017-02-09 11:17:19 +0100

    Add project files.

---
+++ HelloPartner/HelloPartner.cpp
@@ -0,0 +1,14 @@
+// HelloPartner.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello Meri" << endl;
+    return 0;
+}
+
